6ec1072686bc17208008a2071579f93e2c268d04
Chen <chen24201@gmail.com>
2019-10-01 16:23:38 +0300

    exercise item

---
+++ app/src/main/java/com/example/progresee/views/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.progresee.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.progresee.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
