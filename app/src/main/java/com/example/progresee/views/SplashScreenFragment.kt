7ffd67778e47101e5a498b0dbc6015484c0fc88c
Chen <chen24201@gmail.com>
2019-10-06 18:13:12 +0300

    Some changes

--- app/src/main/java/com/example/progresee/views/SplashScreenFragment.kt
+++ app/src/main/java/com/example/progresee/views/SplashScreenFragment.kt
@@ -1,16 +1,12 @@
 package com.example.progresee.views
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.progresee.R
 import com.example.progresee.databinding.FragmentSplashScreenBinding
